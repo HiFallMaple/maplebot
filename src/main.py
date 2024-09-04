cf38aec48957f03aa62729d2f37a467950d9b973
Rick <raymond90541@gmail.com>
2024-09-04 20:21:59 +0800

    refactor: change parameters from ctx to interaction and update message sending methods
    - Changed parameter references from `ctx` to `interaction`.
    - Replaced `ctx.send` with `interaction.response.send_message()` and `interaction.followup.send()`.

--- src/main.py
+++ src/main.py
@@ -7,10 +7,8 @@ import logging
 from dataclasses import dataclass
 from discord import FFmpegPCMAudio
 from discord.ext import commands
-from discord.ext.commands import Context
 from discord.utils import get
 from collections import defaultdict
-
 
 discord.utils.setup_logging(level=logging.INFO, root=True)
 logger = logging.getLogger(__name__)
@@ -18,7 +16,8 @@ logger = logging.getLogger(__name__)
 # setup i18n
 language_code = config.LOCALE
 logger.info(f"Language code: {language_code}")
-translation = gettext.translation(config.APP_NAME, localedir=config.LOCALEDIR, languages=[language_code])
+translation = gettext.translation(
+    config.APP_NAME, localedir=config.LOCALEDIR, languages=[language_code])
 global _
 _ = translation.gettext
 
@@ -45,18 +44,18 @@ intents.presences = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 
-async def check_command_in_guild(ctx: Context) -> bool:
+async def check_command_in_guild(interaction: discord.Interaction) -> bool:
     """checks if the command is run in a guild"""
-    if isinstance(ctx.channel, discord.TextChannel) and ctx.guild is not None:
+    if isinstance(interaction.channel, discord.TextChannel) and interaction.guild is not None:
         return True
     else:
-        await ctx.send(_("Please run this command in a guild"))
+        await interaction.response.send_message(_("Please run this command in a guild"))
         return False
 
 
-async def join(ctx: Context) -> discord.VoiceClient:
-    channel = ctx.author.voice.channel
-    voice = get(bot.voice_clients, guild=ctx.guild)
+async def join(interaction: discord.Interaction) -> discord.VoiceClient:
+    channel = interaction.user.voice.channel
+    voice = get(bot.voice_clients, guild=interaction.guild)
     if voice and voice.is_connected():
         await voice.move_to(channel)
     else:
@@ -79,136 +78,148 @@ def get_song_metadata(song: str) -> Song:
     )
 
 
-async def play_next(ctx: Context, voice: discord.VoiceClient):
-    guild_id = ctx.guild.id
+async def play_next(interaction: discord.Interaction, voice: discord.VoiceClient):
+    guild_id = interaction.guild.id
     if len(playlist[guild_id]) > 0:
-        current_song[guild_id] = playlist[guild_id].pop()
-        print(f"Playing next song: {current_song[guild_id]}")
+        current_song[guild_id] = playlist[guild_id].pop(0)
 
         voice.play(
-            FFmpegPCMAudio(current_song[guild_id].m3u8_url, **config.FFMPEG_OPTIONS),
-            after=lambda e: bot.loop.create_task(play_next(ctx, voice)),
+            FFmpegPCMAudio(
+                current_song[guild_id].m3u8_url, **config.FFMPEG_OPTIONS),
+            after=lambda e: bot.loop.create_task(
+                play_next(interaction, voice)),
         )
-        await ctx.send(_("Playing the next song in playlist")+f"\n{current_song[guild_id].original_url}")
+        await interaction.followup.send(_("Playing the next song in playlist") + f"\n{current_song[guild_id].original_url}")
     else:
         current_song[guild_id] = None
-        await ctx.send(_("Completed playing all songs"))
+        await interaction.followup.send(_("Completed playing all songs"))
 
 
-@bot.hybrid_command()
-async def play(ctx: Context, song: str):
+@bot.tree.command(name="play", description="Play a song from YouTube")
+async def play(interaction: discord.Interaction, song: str):
     """Play a song from YouTube"""
-    if not await check_command_in_guild(ctx):
+    if not await check_command_in_guild(interaction):
         return
 
-    guild_id: int = ctx.guild.id
+    guild_id: int = interaction.guild.id
 
-    await ctx.defer()
-    voice = await join(ctx)
-    print(f"{voice}, {type(voice)}")
-    song: Song = get_song_metadata(song)
+    # Defers the response to avoid interaction timeout
+    await interaction.response.defer()
 
-    if voice.is_playing():
-        playlist[guild_id].append(song)
-        await ctx.send(_("Added to playlist")+f"\n{song.original_url}")
-    else:
-        current_song[guild_id] = song
-        await ctx.send(_("Added to playlist and playing")+f"\n{song.original_url}")
-        voice.play(
-            FFmpegPCMAudio(song.m3u8_url, **config.FFMPEG_OPTIONS),
-            after=lambda e: bot.loop.create_task(play_next(ctx, voice)),
-        )
+    try:
+        voice = await join(interaction)
+        song: Song = get_song_metadata(song)
+
+        if voice.is_playing():
+            playlist[guild_id].append(song)
+            await interaction.followup.send(_("Added to playlist") + f"\n{song.original_url}")
+        else:
+            current_song[guild_id] = song
+            await interaction.followup.send(_("Added to playlist and playing") + f"\n{song.original_url}")
+            voice.play(
+                FFmpegPCMAudio(song.m3u8_url, **config.FFMPEG_OPTIONS),
+                after=lambda e: bot.loop.create_task(
+                    play_next(interaction, voice)),
+            )
+    except Exception as e:
+        await interaction.followup.send(_("An error occurred: ") + str(e))
 
 
-@bot.hybrid_command()
-async def pause(ctx: Context):
+@bot.tree.command(name="pause", description="Pause the current song")
+async def pause(interaction: discord.Interaction):
     """Pause the current song"""
-    if not await check_command_in_guild(ctx):
+    if not await check_command_in_guild(interaction):
         return
 
-    voice = await join(ctx)
+    voice = get(bot.voice_clients, guild=interaction.guild)
     if voice and voice.is_playing():
         voice.pause()
+        await interaction.response.send_message(_("Paused the current song"))
     else:
-        await ctx.send(_("No song is currently playing"))
+        await interaction.response.send_message(_("No song is currently playing"))
 
 
-@bot.hybrid_command()
-async def insert(ctx: Context, song: str, index: int):
+@bot.tree.command(name="insert", description="Insert a song into the list at a specific index")
+async def insert(interaction: discord.Interaction, song: str, index: int):
     """Insert a song into the list at a specific index"""
-    if not await check_command_in_guild(ctx):
+    if not await check_command_in_guild(interaction):
         return
 
-    guild_id: int = ctx.guild.id
+    guild_id: int = interaction.guild.id
 
-    await ctx.defer()
+    await interaction.response.defer()
     url = get_song_metadata(song)
 
     if index < 0 or index > len(playlist[guild_id]):
-        await ctx.send(_("Invalid index"))
+        await interaction.followup.send(_("Invalid index"))
         return
 
     playlist[guild_id].insert(index, url)
-    await ctx.send(_("Inserted song into list"))
+    await interaction.followup.send(_("Inserted song into list"))
 
 
-@bot.hybrid_command()
-async def resume(ctx: Context):
+@bot.tree.command(name="resume", description="Resume the current song")
+async def resume(interaction: discord.Interaction):
     """Resume the current song"""
-    if not await check_command_in_guild(ctx):
+    if not await check_command_in_guild(interaction):
         return
 
-    voice = await join(ctx)
+    voice = await join(interaction)
     if voice and voice.is_paused():
         voice.resume()
+        await interaction.response.send_message(_("Resumed the current song"))
     else:
-        await ctx.send(_("No song is currently paused"))
+        await interaction.response.send_message(_("No song is currently paused"))
 
 
-@bot.hybrid_command()
-async def stop(ctx: Context):
+@bot.tree.command(name="stop", description="Stop all songs")
+async def stop(interaction: discord.Interaction):
     """Stop all songs"""
-    if not await check_command_in_guild(ctx):
+    if not await check_command_in_guild(interaction):
         return
-    voice = await join(ctx)
-    playlist[ctx.guild.id].clear()
+    voice = await join(interaction)
+    playlist[interaction.guild.id].clear()
     voice.stop()
+    await interaction.response.send_message(_("Stopped all songs"))
 
 
-@bot.hybrid_command()
-async def leave(ctx: Context):
+@bot.tree.command(name="leave", description="Leave the voice channel")
+async def leave(interaction: discord.Interaction):
     """Leave the voice channel"""
-    if not await check_command_in_guild(ctx):
+    if not await check_command_in_guild(interaction):
         return
 
-    voice = await join(ctx)
+    voice = await join(interaction)
     if voice and voice.is_connected():
-        playlist[ctx.guild.id].clear()
+        playlist[interaction.guild.id].clear()
         await voice.disconnect()
+        await interaction.response.send_message(_("Left the voice channel"))
     else:
-        await ctx.send(_("The bot is not connected to a voice channel"))
+        await interaction.response.send_message(_("The bot is not connected to a voice channel"))
 
 
-@bot.hybrid_command()
-async def skip(ctx: Context):
+@bot.tree.command(name="skip", description="Skip the current song")
+async def skip(interaction: discord.Interaction):
     """Skip the current song"""
-    if not await check_command_in_guild(ctx):
+    if not await check_command_in_guild(interaction):
         return
-
-    voice = await join(ctx)
-    voice.stop()
-    await play_next(ctx, voice)
-
-
-@bot.hybrid_command()
-async def list(ctx: Context):
-    """Display the current list"""
-    if not await check_command_in_guild(ctx):
-        return
-
-    guild_id: int = ctx.guild.id
+    guild_id: int = interaction.guild.id
     if not current_song[guild_id]:
-        await ctx.send(_("Playlist is empty"))
+        await interaction.response.send_message(_("Playlist is empty"))
+    await interaction.response.send_message(_("Skip") + f" - [{current_song[guild_id].title}]({current_song[guild_id].original_url})")
+    voice = await join(interaction)
+    voice.stop()
+
+
+@bot.tree.command(name="list", description="Display the current list")
+async def list(interaction: discord.Interaction):
+    """Display the current list"""
+    if not await check_command_in_guild(interaction):
+        return
+
+    guild_id: int = interaction.guild.id
+    if not current_song[guild_id]:
+        await interaction.response.send_message(_("Playlist is empty"))
         return
 
     embed = discord.Embed(
@@ -225,15 +236,15 @@ async def list(ctx: Context):
         color=discord.Color.blue(),
     )
 
-    embed.set_footer(text=f"{_('Total songs in playlist')}: {len(playlist[guild_id])}")
-    await ctx.send(embed=embed)
+    embed.set_footer(
+        text=f"{_('Total songs in playlist')}: {len(playlist[guild_id])}")
+    await interaction.response.send_message(embed=embed)
 
 
 @bot.command()
-async def sync(ctx: Context):
+async def sync(ctx: commands.Context):
     sync_result = await bot.tree.sync()
     print(f"Sync result: {sync_result}")
-    print(discord.Object(id=ctx.guild.id))
     await ctx.send(_("Synced!"))
 
 if __name__ == "__main__":
